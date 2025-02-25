0fc61c7c52b0f3bc0198b061d350f7d8f3a48b15
sahithi1101 <sahithi1510@gmail.com>
2025-02-25 10:23:35 -0500

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,26 +17,24 @@ const Footer = () => {
         <div className="footer-column">
           <h5>Our Social</h5>
           <ul>
-            <li><a href="#facebook">Facebook</a></li>
-            <li><a href="#twitter">Twitter</a></li>
-            <li><a href="#instagram">Instagram</a></li>
-            <li><a href="#youtube">YouTube</a></li>
-            <li><a href="#tiktok">TikTok</a></li>
-            <li><a href="#linkedin">LinkedIn</a></li>
+            <li><a href="https://www.facebook.com">Facebook</a></li>
+            <li><a href="https://www.Twitter.com">Twitter</a></li>
+            <li><a href="https://instagram.com">Instagram</a></li>
+            <li><a href="https://www.youtube.com">YouTube</a></li>
+            <li><a href="https://www.tiktok.com">TikTok</a></li>  
           </ul>
         </div>
 
         <div className="footer-column">
           <h5>BookNest is available on</h5>
           <ul>
-            <li><a href="#itunes">iTunes Store</a></li>
-            <li><a href="#amazonPlay">Amazon Play Store</a></li>
-            <li><a href="#googlePlay">Google Play Store</a></li>
+            <li><a href="https://www.itunes.com">iTunes Store</a></li>
+            <li><a href="https://www.googleplay.com">Google Play Store</a></li>
           </ul>
         </div>
     </div>
      
-      {/* Bottom Section */}
+  
       <div className="footer-bottom">
         <p>©2025. All rights reserved MERN Project.</p>
       </div>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import '../style/LoginPage.css';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // const navigate = useNavigate();
+  
 
 const handleSubmit = (e) => {
     e.preventDefault();
